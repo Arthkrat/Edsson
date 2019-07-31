1d3dc0a56b0bf949125e6b6c0831026110c9a6d7
Arthkrat <42123435+Arthkrat@users.noreply.github.com>
2019-07-31 12:27:28 +0300

    Add files via upload

--- src/components/AppConfig.vue
+++ src/components/AppConfig.vue
@@ -1,5 +1,19 @@
 <template>
 <v-container>
+    <v-list-item-content class="ma-auto text-center text-capitalize " >
+        <v-list-item-title class="display-1">
+         Configure Your Layout!
+        </v-list-item-title>
+        <v-list-item-subtitle class="title">
+            Press right plus button to add column
+        </v-list-item-subtitle>
+        <v-list-item-subtitle class="title">
+            Press bottom plus button to add row
+        </v-list-item-subtitle>
+        <v-list-item-subtitle class="title">
+            Press SAVE to get your layout!
+        </v-list-item-subtitle>
+    </v-list-item-content>
     <v-flex justify-center>
         <v-btn class="px-15 mx-auto" dark color="teal lighten-1" elevation="5" block @click="saveValues">{{ getConfig.actions[0].label }}</v-btn> 
     </v-flex>
@@ -8,7 +22,7 @@
             <div v-for="(element, index) in col.elements" :key=index >  
                 <v-text-field class="pa-0" label=id type="text" v-model="element.id">  </v-text-field>
                 <v-select class="pa-0" v-model="element.type" :items="types" label="type" ></v-select> 
-                <v-text-field class="pa-0" label=Value type="text" v-model="element.defaultValue" @keypress="validate($event, col.elements, index)"></v-text-field>
+                <v-text-field class="pa-0" label=Value :type="element.type" v-model="element.defaultValue" @keydown="validate($event, col.elements, index)"></v-text-field>
             </div>
           
             </v-col>
@@ -49,7 +63,7 @@ export default {
             },
             validate($event, elements, index){
                 elements[index].type=="number" ? $event.target.value = $event.target.value.replace(/[^0-9]/g,"")
-                : $event.target.value = $event.target.value.replace(/[^a-zA-Z]/g,"") ;
+                : true ;
             }
         }
   /*  created() {
